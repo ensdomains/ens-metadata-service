94e5bf6a249b4521544606953c49b6e1ef83f740
Makoto Inoue <2630+makoto@users.noreply.github.com>
2021-06-18 15:49:21 +0100

    Support both INT and Hex tokenId

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import { gql } from 'graphql-request'
 import { request } from 'graphql-request'
 import { nextTick } from 'process';
-const btoa = require('btoa');
-const URL = 'http://127.0.0.1:8000/subgraphs/name/graphprotocol/ens'
-const eth = '0x93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae'
+import { ethers } from "ethers";
 
+const btoa = require('btoa');
+// const URL = 'http://127.0.0.1:8000/subgraphs/name/graphprotocol/ens'
+const URL = 'https://api.thegraph.com/subgraphs/name/makoto/ensrinkeby'
+const eth = '0x93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae'
 function getImage(name:string){
   return `
   <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
@@ -54,7 +56,6 @@ export const GET_REGISTRATIONS = gql`
     }  
   }
 `
-
 const app = express();
 
 app.get('/', (req, res) => {
@@ -62,10 +63,19 @@ app.get('/', (req, res) => {
 })
 
 app.get('/name/:tokenId', async function (req, res) {
-  const { tokenId } = req.params
-  console.log({tokenId})
+  let { tokenId } = req.params
+  console.log(1, {tokenId})
+  let hexId, intId
+  if(!tokenId.match(/^0x/)){
+    intId = tokenId
+    hexId = ethers.utils.hexValue(ethers.BigNumber.from(tokenId))
+  }else{
+    intId = ethers.BigNumber.from(tokenId).toString()
+    hexId = tokenId
+  }
+  console.log(2, {intId, hexId})
   try{
-    const {domain:{name, labelName, labelhash, createdAt, owner, parent}} = await request(URL, GET_DOMAINS, { tokenId })
+    const {domain:{name, labelName, labelhash, createdAt, owner, parent}} = await request(URL, GET_DOMAINS, { tokenId:hexId })
     console.log({name, labelName, labelhash, createdAt, owner, parent})
     const imageUrl = 'data:image/svg+xml;base64,'+ btoa(getImage(name))
     let attributes = [
@@ -107,6 +117,7 @@ app.get('/name/:tokenId', async function (req, res) {
   }
 })
 
-app.listen(3000, () => {
-  console.log('The application is listening on port 3000!');
-})
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`APP_LOG::App listening on port ${PORT}`);
+});
