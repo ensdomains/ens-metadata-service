dba9e0ddb859b41516453d148e6f56efc769091a
Muhammed Tanrikulu <md.tanrikulu@gmail.com>
2021-09-06 20:58:45 +0200

    make cors default

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,9 @@ import endpoints         from './endpoint';
 
 
 const app = express();
+app.use(cors());
 
 if (process.env.ENV === 'local') {
-  app.use(cors());
   app.use('/assets', express.static(path.join(__dirname, 'assets')));
 }
 
