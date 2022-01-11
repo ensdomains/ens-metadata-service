64b5682f5b3c8310d6269de22bef3702a1e0ed50
Muhammed Tanrıkulu <md.tanrikulu@gmail.com>
2022-01-11 18:51:35 +0100

    add compression middleware

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import path                                         from 'path';
 import cors                                         from 'cors';
+import compression                                  from 'compression';
 import express, { Request, Response, NextFunction } from 'express';
 import docUI                                        from 'redoc-express';
 
 import endpoints                                    from './endpoint';
 
-const setCacheHeader = function (req: Request, res: Response, next: NextFunction) {
+const setCacheHeader = function (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   const period = 60 * 60;
 
   if (req.method == 'GET') {
@@ -27,6 +32,18 @@ if (process.env.ENV === 'local') {
 // apply cache header for all get requests
 app.use(setCacheHeader);
 endpoints(app);
+
+app.use(compression({ filter: shouldCompress }));
+
+function shouldCompress(req: Request, res: Response) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false;
+  }
+
+  // fallback to standard filter function
+  return compression.filter(req, res);
+}
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
