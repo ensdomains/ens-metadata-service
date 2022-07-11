81ab988519ad81e5e2007d283f4d3468fc489102
tate <tate@ens.domains>
2022-07-11 10:16:44 +1000

    add dockerfile + USE_LOCAL_WEB3 env var

--- src/service/network.ts
+++ src/service/network.ts
@@ -14,32 +14,37 @@ export default function getNetwork(network: string): any {
   // we will have namewrapper support and more attributes when latest subgraph goes to production
   let SUBGRAPH_URL: string;
   let CLOUDFLARE_WEB3_URL: string = 'https://web3metadata.ens.domains/v1';
-  switch (network) {
-    case NETWORK.LOCAL:
-      SUBGRAPH_URL = 'http://127.0.0.1:8000/subgraphs/name/graphprotocol/ens';
-      CLOUDFLARE_WEB3_URL = `${CLOUDFLARE_WEB3_URL}/${NETWORK.RINKEBY}`;
-      break;
-    case NETWORK.RINKEBY:
-      SUBGRAPH_URL =
-        'https://api.thegraph.com/subgraphs/name/makoto/ensrinkeby';
-      CLOUDFLARE_WEB3_URL = `${CLOUDFLARE_WEB3_URL}/${NETWORK.RINKEBY}`;
-      break;
-    case NETWORK.ROPSTEN:
-      SUBGRAPH_URL =
-        'https://api.thegraph.com/subgraphs/name/ensdomains/ensropsten';
-      CLOUDFLARE_WEB3_URL = `${CLOUDFLARE_WEB3_URL}/${NETWORK.ROPSTEN}`;
-      break;
-    case NETWORK.GOERLI:
-      SUBGRAPH_URL =
-        'https://api.thegraph.com/subgraphs/name/ensdomains/ensgoerli';
-      CLOUDFLARE_WEB3_URL = `${CLOUDFLARE_WEB3_URL}/${NETWORK.GOERLI}`;
-      break;
-    case NETWORK.MAINNET:
-      SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/ensdomains/ens';
-      CLOUDFLARE_WEB3_URL = `${CLOUDFLARE_WEB3_URL}/${NETWORK.MAINNET}`;
-      break;
-    default:
-      throw new UnsupportedNetwork(`Unknown network '${network}'`, 400);
+  if (process.env.USE_LOCAL_WEB3) {
+    SUBGRAPH_URL = 'http://127.0.0.1:8000/subgraphs/name/graphprotocol/ens';
+    CLOUDFLARE_WEB3_URL = 'http://127.0.0.1:8545';
+  } else {
+    switch (network) {
+      case NETWORK.LOCAL:
+        SUBGRAPH_URL = 'http://127.0.0.1:8000/subgraphs/name/graphprotocol/ens';
+        CLOUDFLARE_WEB3_URL = `${CLOUDFLARE_WEB3_URL}/${NETWORK.RINKEBY}`;
+        break;
+      case NETWORK.RINKEBY:
+        SUBGRAPH_URL =
+          'https://api.thegraph.com/subgraphs/name/makoto/ensrinkeby';
+        CLOUDFLARE_WEB3_URL = `${CLOUDFLARE_WEB3_URL}/${NETWORK.RINKEBY}`;
+        break;
+      case NETWORK.ROPSTEN:
+        SUBGRAPH_URL =
+          'https://api.thegraph.com/subgraphs/name/ensdomains/ensropsten';
+        CLOUDFLARE_WEB3_URL = `${CLOUDFLARE_WEB3_URL}/${NETWORK.ROPSTEN}`;
+        break;
+      case NETWORK.GOERLI:
+        SUBGRAPH_URL =
+          'https://api.thegraph.com/subgraphs/name/ensdomains/ensgoerli';
+        CLOUDFLARE_WEB3_URL = `${CLOUDFLARE_WEB3_URL}/${NETWORK.GOERLI}`;
+        break;
+      case NETWORK.MAINNET:
+        SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/ensdomains/ens';
+        CLOUDFLARE_WEB3_URL = `${CLOUDFLARE_WEB3_URL}/${NETWORK.MAINNET}`;
+        break;
+      default:
+        throw new UnsupportedNetwork(`Unknown network '${network}'`, 400);
+    }
   }
   const provider = new ethers.providers.StaticJsonRpcProvider(CLOUDFLARE_WEB3_URL);
   return { CLOUDFLARE_WEB3_URL, SUBGRAPH_URL, provider };
