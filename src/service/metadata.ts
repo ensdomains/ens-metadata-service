748829a931db32b27511dec9f8c81db14e6e7aee
Muhammed Tanrıkulu <md.tanrikulu@gmail.com>
2023-12-11 02:10:05 +0100

    debug if noto font is the cause for segfault

--- src/service/metadata.ts
+++ src/service/metadata.ts
@@ -13,13 +13,13 @@ import { getCodePointLength, getSegmentLength } from '../utils/charLength';
 const { createCanvas, registerFont } = require('canvas');
 
 
-// try {
-//   registerFont(CANVAS_FONT_PATH, { family: 'Satoshi' });
-//   registerFont(CANVAS_EMOJI_FONT_PATH, { family: 'Noto Color Emoji' });
-// } catch(error) {
-//   console.warn("Font registeration is failed.");
-//   console.warn(error);
-// }
+try {
+  registerFont(CANVAS_FONT_PATH, { family: 'Satoshi' });
+  // registerFont(CANVAS_EMOJI_FONT_PATH, { family: 'Noto Color Emoji' });
+} catch(error) {
+  console.warn("Font registeration is failed.");
+  console.warn(error);
+}
 
 
 export interface MetadataInit {
